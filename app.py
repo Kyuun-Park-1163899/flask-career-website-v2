bdcec1f130847c4ae440f5deeee64878b0b34496
Agnes Kyuun Park <agnespark0413@gmail.com>
2025-01-03 23:35:48 +0000

    Render dynamic data using Jinja template tags

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title':'Data Analyst',
+    'location':'Seoul, Korea',
+    'salary':'$2000'
+  },
+  {
+    'id': 2,
+    'title':'Data Scientist',
+    'location':'Busan, Korea',
+  },
+  {
+    'id': 3,
+    'title':'Web Developer',
+    'location':'Christchurch, New Zealand',
+    'salary':'$4000'
+  },
+  {
+    'id': 4,
+    'title':'IT Manager',
+    'location':'Auckland, New Zealand',
+    'salary':'$5000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
