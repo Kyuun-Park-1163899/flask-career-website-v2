6d9ee4d5c125324443da02881b029e8f57e03b6b
Agnes Kyuun Park <agnespark0413@gmail.com>
2025-01-04 10:53:11 +0000

    save applications to DB

--- database.py
+++ database.py
@@ -14,11 +14,36 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
     return jobs
 
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {"val": id})
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
       return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+  query = text("""
+      INSERT INTO applications (
+          job_id, full_name, email, linkedin, education, work_experience, resume
+      ) VALUES (
+          :job_id, :full_name, :email, :linkedin, :education, :work_experience, :resume
+      )
+  """)
+
+  # 데이터베이스와 연결 및 트랜잭션 시작
+  with engine.begin() as conn:
+    conn.execute(
+        query, {
+            "job_id": job_id,
+            "full_name": data.get('full_name'),
+            "email": data.get('email'),
+            "linkedin": data.get('linkedin'),
+            "education": data.get('education'),
+            "work_experience": data.get('work_experience'),
+            "resume": data.get('resume')
+        })
